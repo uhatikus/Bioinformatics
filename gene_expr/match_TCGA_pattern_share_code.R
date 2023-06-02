6bf647719fdc1fbcf7fe97f919fada05681c47cc
Ukhatov Alexander <uhatov007@yandex.ru>
2023-06-02 19:57:13 +0300

    pretty graph

--- gene_expr/match_TCGA_pattern_share_code.R
+++ gene_expr/match_TCGA_pattern_share_code.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(openxlsx)
 library(magrittr)
 library(readxl)
+library(ggplot2)
 
 remove_substring_from_list <- function(my_list, substring) {
   modified_list <- lapply(my_list, function(item) {
@@ -43,33 +44,17 @@ match_exp_pattern <- function(gl = gene_list, d = xml_df,
   d[["bcr_patient_barcode"]][which(matched_samples)]
 }
 
-gene_list <- unlist(as.list(read_excel("gene_list.xlsx", col_names = FALSE)))
-tcga_data <- read.table("tcga_lgg_data.txt", header = TRUE, sep = "\t")
-# print(tcga_data)
-diff_exp_data <- read_excel("expression_data.xlsx", col_names = TRUE)
-# print(diff_exp_data)
-diff_exp_data <- diff_exp_data[!is.na(diff_exp_data[, 1]), ]
-diff_exp_data <- diff_exp_data[!is.na(diff_exp_data[, 2]), ]
-for (cutoff_value in c(0.5, 0.6, 0.7, 0.8, 0.85, 0.9)) {
-  print("cutoff:")
-  print(cutoff_value)
-  matched_samples <- match_exp_pattern(
-    gl = gene_list, # from one of the tabs in gene_list.xlsx
-    d = tcga_data, # from tcga_lgg_data.txt
-    de = diff_exp_data, # from the corresponding list of expression_data.xlsx
-    cutoff = cutoff_value
-  ) # cutoff - the minimal fraction of genes with matching pattern within selected sample
-  # print(matched_samples)
-
-  tcga_data_matched <- tcga_data[!tcga_data$bcr_patient_barcode %in% matched_samples, ]
-  row.names(tcga_data_matched) <- tcga_data_matched$bcr_patient_barcode
-  all_gene_median_columns <- colnames(tcga_data_matched)[grep("median", colnames(tcga_data_matched))]
+analyse_sample_data <- function(patient_data_, diff_exp_data_) {
+  patient_data_ <- copy(patient_data_)
+  diff_exp_data_ <- copy(diff_exp_data_)
+  row.names(patient_data_) <- patient_data_$bcr_patient_barcode
+  all_gene_median_columns <- colnames(patient_data_)[grep("median", colnames(patient_data_))]
   all_gene_columns <- remove_substring_from_list(all_gene_median_columns, "median")
 
-  useful_exp_data <- diff_exp_data[diff_exp_data$hgnc_symbol %in% all_gene_columns, ]
+  useful_exp_data <- diff_exp_data_[diff_exp_data_$hgnc_symbol %in% all_gene_columns, ]
   useful_exp_data[useful_exp_data == -1] <- 0
   # print("Not in expression_data.xlsx:")
-  not_in_exp_data <- setdiff(all_gene_columns, useful_exp_data$hgnc_symbol)
+  # not_in_exp_data <- setdiff(all_gene_columns, useful_exp_data$hgnc_symbol)
   # print(not_in_exp_data)
 
   # Transpose the dataframe
@@ -80,25 +65,71 @@ for (cutoff_value in c(0.5, 0.6, 0.7, 0.8, 0.85, 0.9)) {
   # Reset row names
   rownames(useful_exp_data_good_view) <- NULL
 
-  useful_exp_data_for_comparison <- useful_exp_data_good_view[rep(seq_len(nrow(useful_exp_data_good_view)), each = nrow(tcga_data_matched)), ]
+  useful_exp_data_for_comparison <- useful_exp_data_good_view[rep(seq_len(nrow(useful_exp_data_good_view)), each = nrow(patient_data_)), ]
   new_col_names <- paste0(colnames(useful_exp_data_for_comparison), "median")
   colnames(useful_exp_data_for_comparison) <- new_col_names
-  row.names(useful_exp_data_for_comparison) <- tcga_data_matched$bcr_patient_barcode
+  row.names(useful_exp_data_for_comparison) <- patient_data_$bcr_patient_barcode
   useful_exp_data_for_comparison[] <- lapply(useful_exp_data_for_comparison, function(x) ifelse(is.character(x), as.integer(x), x))
-  tcga_data_matched_for_comparison <- tcga_data_matched[colnames(useful_exp_data_for_comparison)]
+  data_for_comparison <- patient_data_[colnames(useful_exp_data_for_comparison)]
 
-  errors <- sum(abs(tcga_data_matched_for_comparison - useful_exp_data_for_comparison))
-  print("Number of people: ")
-  print(nrow(tcga_data_matched))
-  print("Sum errors: ")
-  print(errors)
-  print("Errors per person: ")
-  print(errors / nrow(tcga_data_matched))
-  print("Errors per gene: ")
-  print(errors / length(useful_exp_data$hgnc_symbol))
-  print("Probability of error: ")
-  print(errors / length(useful_exp_data$hgnc_symbol) / nrow(tcga_data_matched))
+  errors <- abs(data_for_comparison - useful_exp_data_for_comparison)
+  return(errors)
 }
+
+gene_list <- unlist(as.list(read_excel("gene_list.xlsx", col_names = FALSE)))
+tcga_data <- read.table("tcga_lgg_data.txt", header = TRUE, sep = "\t")
+# print(tcga_data)
+diff_exp_data <- read_excel("expression_data.xlsx", col_names = TRUE)
+# print(diff_exp_data)
+diff_exp_data <- diff_exp_data[!is.na(diff_exp_data[, 1]), ]
+diff_exp_data <- diff_exp_data[!is.na(diff_exp_data[, 2]), ]
+
+cutoff_values <- c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.65, 0.7, 0.71, 0.72, 0.73, 0.74, 0.75, 0.8, 0.85, 0.9, 1)
+p_errors_matched <- c()
+p_errors_unmatched <- c()
+# cutoff_values <- c(0.7, 0.8)
+
+for (cutoff_value in cutoff_values) {
+  # print("cutoff:")
+  # print(cutoff_value)
+  matched_samples <- match_exp_pattern(
+    gl = gene_list, # from one of the tabs in gene_list.xlsx
+    d = tcga_data, # from tcga_lgg_data.txt
+    de = diff_exp_data, # from the corresponding list of expression_data.xlsx
+    cutoff = cutoff_value
+  ) # cutoff - the minimal fraction of genes with matching pattern within selected sample
+  tcga_data_matched <- tcga_data[tcga_data$bcr_patient_barcode %in% matched_samples, ]
+  errors_matched <- analyse_sample_data(tcga_data_matched, diff_exp_data)
+  p_error <- sum(errors_matched) / length(useful_exp_data$hgnc_symbol) / nrow(tcga_data_matched)
+  p_errors_matched <- c(p_errors_matched, p_error)
+
+  print(cutoff_value)
+  strange_genes <- colnames(errors_matched[colSums(errors_matched) > 0.5 * nrow(errors_matched)])
+  if (length(strange_genes) < 20) {
+    print(length(strange_genes))
+    print(gsub("median", "", strange_genes))
+  }
+  strange_people <- rownames(errors_matched[rowSums(errors_matched) > 0.5 * ncol(errors_matched), ])
+  print(length(strange_people) == 0)
+
+  tcga_data_unmatched <- tcga_data[!tcga_data$bcr_patient_barcode %in% matched_samples, ]
+  errors_unmatched <- analyse_sample_data(tcga_data_unmatched, diff_exp_data)
+  p_error <- sum(errors_unmatched) / length(useful_exp_data$hgnc_symbol) / nrow(tcga_data_unmatched)
+  p_errors_unmatched <- c(p_errors_unmatched, p_error)
+}
+# p <- ggplot(data = data.frame(cutoff_values, p_errors_matched), aes(x = cutoff_values, y = p_errors_matched)) +
+#   geom_point(color = "blue") +
+#   geom_smooth(method = "lm", se = FALSE, color = "red", linetype = "dashed") +
+#   labs(title = "Scatter Plot with Trend Line", x = "X", y = "Y") +
+#   theme_minimal()
+p <- ggplot() +
+  geom_point(data = data.frame(x = cutoff_values, y = p_errors_matched), aes(x = cutoff_values, y = p_errors_matched), color = "blue") +
+  geom_point(data = data.frame(x = cutoff_values, y = p_errors_unmatched), aes(x = cutoff_values, y = p_errors_unmatched), color = "green") +
+  geom_smooth(data = data.frame(x = cutoff_values, y = p_errors_matched), aes(x = cutoff_values, y = p_errors_matched), method = "lm", se = FALSE, color = "red", linetype = "dashed") +
+  geom_smooth(data = data.frame(x = cutoff_values, y = p_errors_unmatched), aes(x = cutoff_values, y = p_errors_unmatched), method = "lm", se = FALSE, color = "purple", linetype = "dashed") +
+  labs(title = "Scatter Plot with Two Sets of Points", x = "X", y = "Y") +
+  theme_minimal()
+IRdisplay::display(p)
 
 # R t-test tcga_data_matched vs tcga_data_others
 # tcga_data_matched <- tcga_data[tcga_data$bcr_patient_barcode %in% matched_samples, ]
